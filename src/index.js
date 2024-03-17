5521e1adf2b1a1ba61ff44ffc1328e334f08c6ea
exit <phirom.kong@student.cadt.edu.kh>
2024-03-17 21:56:50 +0700

    Fix dependencies confilct

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// Use ReactDOM.render for React 17
+ReactDOM.render(
+ <React.StrictMode>
     <App />
-  </React.StrictMode>
+ </React.StrictMode>,
+ document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
