f5b63a0efd37f23f7b7d75b862d3906874ab7223
exit <phirom.kong@student.cadt.edu.kh>
2024-03-19 09:30:16 +0700

    Debug i18next configuration

--- src/i18next.js
+++ src/i18next.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'English',
+    fallbackLng: 'en',
     debug: true,
     interpolation: {
       escapeValue: false,
